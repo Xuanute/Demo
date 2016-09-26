0047865e01fce803bec307e6e38eefc59d98dbba
trinh <trinh@Me>
2016-09-26 09:52:34 +0700

    test

---
+++ TestPro/demotest/Data.java
@@ -0,0 +1,10 @@
+package demotest;
+
+public class Data {
+	int data[] = {0, 12, 45};
+	
+	public void display(){
+		System.out.println("To do implement data display");
+	}
+
+}
---
+++ TestPro/demotest/Main.java
@@ -0,0 +1,19 @@
+package demotest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Data data = new Data();
+		displayScreen();
+		data.display();
+	}
+	public static void displayScreen()
+	{
+		System.out.println("hello 1");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+		System.out.println("new message");
+		
+	}
+
+}
